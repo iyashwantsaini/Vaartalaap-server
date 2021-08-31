d3b6612736c1ee2951ea02cedaca538ec3d1e6f9
meyash <yashsn2127@gmail.com>
2021-08-31 14:27:32 +0530

    socket-error-fixed

--- schema/TextEditorData.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const TextEditorData = new Schema({
-  _id: String,
-  data: Object,
-});
-
-module.exports = model("TextEditorData", TextEditorData);
